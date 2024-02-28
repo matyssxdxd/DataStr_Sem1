0f74bdb0e9f8dbc1264da20fed07134f4a6c0f01
Matīss Purviņš <s22purvmati@vea.lv>
2024-02-28 09:45:02 +0200

    Created MyArrayList constructors and isEmpty, isFull, howManyElements methods.

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,4 +1,33 @@
 package datastr;
 
 public class MyArrayList {
+
+    private int[] list;
+    private final int LIST_DEFAULT_SIZE = 10;
+    private int size = LIST_DEFAULT_SIZE;
+    private int counter = 0;
+
+    public MyArrayList() {
+        list = new int[this.size];
+    }
+
+    public MyArrayList(int size) {
+        if (size > 0) {
+            this.size = size;
+        }
+
+        list = new int[this.size];
+    }
+
+    public boolean isEmpty() {
+        return counter == 0;
+    }
+
+    public boolean isFull() {
+        return counter == size;
+    }
+
+    public int howManyElements() {
+        return counter;
+    }
 }
